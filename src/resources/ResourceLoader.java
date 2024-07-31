879c1f2a3827e183ee464e7401764057f29e174b
Laurin Trapp <18750@nordakademie.de>
2024-07-31 18:08:26 +0200

    Just some tests

--- src/resources/ResourceLoader.java
+++ src/resources/ResourceLoader.java
@@ -3,18 +3,10 @@ package resources;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
 import org.lwjgl.BufferUtils;
@@ -43,7 +35,7 @@ public class ResourceLoader {
 	public static String loadShaderAsString(String parent, String fileName) {
 		try {
 			return new String(loadShader(parent, fileName).readAllBytes());
-		} catch (IOException e) {
+		} catch (IOException e) { 
 			e.printStackTrace();
 		}
 		return null;
@@ -63,95 +55,15 @@ public class ResourceLoader {
 			return null;
 		}
 	}
-
-	public static ArrayList<String> loadObjFile(String parentFolder, String fileName) {
-		InputStream modelFile = ResourceLoader.class
-				.getResourceAsStream("models" + File.separator + parentFolder + File.separator + fileName);
-		try {
-			String content = new String(modelFile.readAllBytes());
-			ArrayList<String> list = new ArrayList<>();
-			for (String line : content.split("\n")) {
-				list.add(line);
-			}
-			return list;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static ArrayList<String> loadMaterialFile(String parentFolder, String fileName) {
-		InputStream modelFile = ResourceLoader.class.getResourceAsStream("models" + File.separator + parentFolder + File.separator + fileName);
-		try {
-			String content = new String(modelFile.readAllBytes());
-			ArrayList<String> list = new ArrayList<>();
-			for (String line : content.split("\n")) {
-				list.add(line);
-			}
-			return list;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static File getFile(String parent, String file) {
-		try (InputStream is = rl.getClass().getResourceAsStream(parent + File.separator + file);) {
-			return getFileFromStream(is);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static File getFileFromStream(InputStream is) {
-
-		File tempFolder = new File("temp");
-		tempFolder.mkdir();
-		File tempFile = new File(tempFolder, UUID.randomUUID().toString());
-
-		try (OutputStream os = new FileOutputStream(tempFile);) {
-			tempFile.createNewFile();
-			
-			byte[] buffer = is.readAllBytes();
-
-			os.write(buffer);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return tempFile;
+	
+	public static File getResourceFile(String resFolder, String parentFolder, String fileName) {
+		URL url = rl.getClass().getResource(resFolder + File.separator + parentFolder + File.separator + fileName);
+		File file = new File(url.getFile());
+		
+		return file;
 	}
 
 	public static File getModelFile(String parent, String file) {
-		try (InputStream is = rl.getClass()
-				.getResourceAsStream("models" + File.separator + parent + File.separator + file);) {
-
-			return getFileFromStream(is);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return getResourceFile("models", parent, file);
 	}
-
-	public static void clear() {
-		File file = new File("temp");
-		deleteChildFiles(file);
-		file.delete();
-	}
-
-	private static void deleteChildFiles(File root) {
-		if (root.isDirectory()) {
-			for (File file : root.listFiles()) {
-				deleteChildFiles(file);
-			}
-		}
-		root.delete();
-	}
-
-//	public static void main(String[] args) {
-//		System.out.println(loadTexture("skybox/back.png"));
-//
-//	}
-
 }
